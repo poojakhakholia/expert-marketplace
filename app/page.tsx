2f8f34286e2d8ab8590a1bb4313dba225ad19638
poojakhakholia <poojakhakholia@gmail.com>
2026-01-15 13:01:38 +0530

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Expert Marketplace</h1>
+      <p>Your platform to book 1-on-1 expert sessions.</p>
+    </main>
+  );
+}
