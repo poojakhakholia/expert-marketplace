eb029154ba34634c471a976556affee329a144b6
poojakhakholia <poojakhakholia@gmail.com>
2026-01-15 16:31:14 +0530

    Update page.tsx

--- app/experts/page.tsx
+++ app/experts/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { supabase } from "@/lib/supabase";
+import { supabase } from "../../lib/supabase";
 
 export default async function Experts() {
   const { data } = await supabase
